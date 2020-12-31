6cb96e4f4f487f1df33d325cb57440bcda7f85ca
Taisei-rgb <taisei.contact@gmail.com>
2020-12-31 14:58:39 +0900

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,8 +72,11 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago 
   end
 
-  def feed 
-    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                    WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                    OR user_id = :user_id", user_id: id)
   end
 
   def follow(other_user)
